beb81d7166180a17072612431d54ecd63abcd9fb
twhuang <twh760812@gmail.com>
2018-07-27 11:29:00 -0500

    updated core

--- ot/config.hpp
+++ ot/config.hpp
@@ -2,6 +2,6 @@
 #define OT_VERSION_MINOR 0
 #define OT_VERSION_PATCH 0
 #define OT_VERSION "2.0.0 (alpha)"
-#define OT_HOME "/Users/twhuang/PhD/Code/OpenTimer"
+#define OT_HOME "/home/twhuang/PhD/Code/OpenTimer"
 #define OT_INSTALL_PREFIX "/usr/local"
 #define OT_TCLSH_PATH "/usr/bin/tclsh"
