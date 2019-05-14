476be29458b46c1da94efeaf1ccd404149608769
twhuang <twh760812@gmail.com>
2019-05-14 14:07:03 -0500

    remove config.hpp

--- ot/config.hpp
+++
@@ -1,8 +0,0 @@
-#define OT_VERSION_MAJOR 2
-#define OT_VERSION_MINOR 0
-#define OT_VERSION_PATCH 0
-#define OT_VERSION "2.0.0 (alpha)"
-#define OT_HOME "/home/twhuang/PhD/Code/OpenTimer"
-#define OT_HOST_SYSTEM "Linux-4.15.0-47-generic"
-#define OT_INSTALL_PREFIX "/usr/local"
-#define OT_TCLSH_PATH "/usr/bin/tclsh"
